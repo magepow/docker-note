beca8059e07892d75688a84a0b4daca8425867d7
Alex Dong <team.magiccart@gmail.com>
2022-12-13 23:06:43 +0700

    Create command.sh

---
+++ command.sh
@@ -0,0 +1,12 @@
+I. Docker:
+
+II. Docker-compose
+
+1. Run command in docker-compose
+
+docker-compose up: start Compose
+
+docker-compose status: status Compose
+
+
+docker-compose exec web bash: access to terminal command in service web
