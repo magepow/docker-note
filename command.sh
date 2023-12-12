615e298a09674cfaf916e7591da56e496567b061
Alex Dong <team.magiccart@gmail.com>
2023-12-12 23:03:48 +0700

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,8 +1,23 @@
 I. Docker:
 
+```
+#docker list
+docker ps -as
+docker image ls
+docker volume ls
+docker network ls
+#docker remove
+docker kill $(docker ps -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
+docker image prune -a
+
+```
 II. Docker-compose
 
 1. Run command in docker-compose
+
+```
 
 docker-compose up: start Compose
 
@@ -10,3 +25,5 @@ docker-compose status: status Compose
 
 
 docker-compose exec web bash: access to terminal command in service web
+
+```
